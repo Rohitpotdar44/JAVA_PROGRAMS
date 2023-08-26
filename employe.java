1efc8cdfe9284e7658e060960b285b5da3ccbba2
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-08-26 14:01:20 +0530

    java code

---
+++ employe.java
@@ -0,0 +1,15 @@
+/**
+ * employe
+ */
+import java.io.*;
+public class employe {
+public static void main(String[] args) {
+    String name="hemant";
+    System.out.println("employe name is:"+name);
+
+    
+}
+
+
+    
+}
