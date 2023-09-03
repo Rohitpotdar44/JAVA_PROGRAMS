d09d3b520f63623376ff01b2e8f8e9642e0348bc
Rohitpotdar44 <potdarrohit59@gmail.com>
2023-09-03 17:37:51 +0530

    VariablesCasting

---
+++ ConstantDemo.java
@@ -0,0 +1,11 @@
+public class ConstantDemo 
+{
+ public static void main(String[] args) 
+ {
+    final String collegeName="PILLAI HOC";                   //constant variable used
+    System.out.println("My College Name Is ="+ collegeName);
+    /*collegeName="MGM";
+    System.out.println("My College Name Is ="+ collegeName);*/
+
+ }    
+}
